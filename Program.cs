95a05f1288872775342387c54f2b1d73db629c75
Haihua Xu <xuhaihua@HaihuamatoAir.bad>
2017-08-03 21:33:19 -0700

    New Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KaraokeCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
